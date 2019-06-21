11ab6104550f6cf24c6f05b068e7653e42b263d0
easbar <easbar.mail@posteo.net>
2019-06-21 11:20:42 +0200

    Handles u-turns in DefaultTurnCostHandler.

--- core/src/main/java/com/graphhopper/routing/weighting/DefaultTurnCostHandler.java
+++ core/src/main/java/com/graphhopper/routing/weighting/DefaultTurnCostHandler.java
@@ -1,18 +1,30 @@
 package com.graphhopper.routing.weighting;
 
+import com.graphhopper.util.EdgeIterator;
+
 public class DefaultTurnCostHandler implements TurnCostHandler {
+
+    private double defaultUTurnCost = Double.POSITIVE_INFINITY;
+
     @Override
     public double calcTurnWeight(int inEdge, int viaNode, int outEdge) {
-        return 0;
+        if (inEdge == EdgeIterator.NO_EDGE || outEdge == EdgeIterator.NO_EDGE) {
+            return 0;
+        }
+        return inEdge == outEdge ? defaultUTurnCost : 0;
     }
 
     @Override
     public long calcTurnMillis(int inEdge, int viaNode, int outEdge) {
-        return 0;
+        double turnWeight = calcTurnWeight(inEdge, viaNode, outEdge);
+        if (Double.isInfinite(turnWeight)) {
+            return Long.MAX_VALUE;
+        }
+        return 1000 * (long) turnWeight;
     }
 
     @Override
     public void setDefaultUTurnCost(double uTurnCosts) {
-        throw new IllegalArgumentException("You cannot set default u-turn costs on a DefaultTurnCostHandler");
+        defaultUTurnCost = uTurnCosts;
     }
 }
